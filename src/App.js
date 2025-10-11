e95deb4432065f56fc516fec58b9efe21428b839
Jerry-Lee Somera <66772527+LoudMouthLemon@users.noreply.github.com>
2025-10-11 19:35:57 -0400

    Dep inclusions (#3)
    * Github action for github pages added
    
    * Name Changed

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101521229 || Jerry-Lee Somera || DevOps</h5>
+      <h5>Developed By: 101521229 || Jerry-Lee Somera || COMP3104 || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
